68a1f3ee02cfaf27b1ee32fbc080e033abeab62c
Benjamin Oakes <ben@benjaminoakes.com>
2011-05-22 18:15:27 -0400

    Docs for Maid::Tools

--- lib/maid/tools.rb
+++ lib/maid/tools.rb
@@ -2,7 +2,17 @@ require 'fileutils'
 require 'find'
 require 'time'
 
+# Collection of utility methods included in Maid::Maid (and thus available in the rules DSL).
+#
+# In general, all paths are automatically expanded (e.g. '~/Downloads/foo.zip' becomes '/home/username/Downloads/foo.zip').
 module Maid::Tools
+  # Move from <tt>from</tt> to <tt>to</tt>.
+  #
+  # The path is not moved if a file already exists at the destination with the same name.  A warning is logged instead.
+  #
+  # This method delegates to FileUtils.  The instance-level <tt>file_options</tt> hash is passed to control the <tt>:noop</tt> option.
+  #
+  #   move('~/Downloads/foo.zip', '~/Archive/Software/Mac OS X/')
   def move(from, to)
     from = File.expand_path(from)
     to = File.expand_path(to)
@@ -15,11 +25,16 @@ module Maid::Tools
       @logger.warn "skipping #{from.inspect} because #{target.inspect} already exists"
     end
   end
- 
+
+  # Move the given path to the trash (as set by <tt>trash_path</tt>).
+  #
+  # The path is moved if a file already exists in the trash with the same name.  However, the current date and time is appended to the filename.
+  #
+  #   trash('~/Downloads/foo.zip')
   def trash(path)
     target = File.join(@trash_path, File.basename(path))
     safe_trash_path = File.join(@trash_path, "#{File.basename(path)} #{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}")
-  
+
     if File.exist?(target)
       move(path, safe_trash_path)
     else
@@ -27,43 +42,75 @@ module Maid::Tools
     end
   end
 
+  # Give all files matching the given glob.
+  #
+  # Delgates to Dir.
+  #
+  #   dir('~/Downloads/*.zip')
   def dir(glob)
     Dir[File.expand_path(glob)]
   end
 
+  # Find matching files, akin to the Unix utility <tt>find</tt>.
+  #
+  # Delegates to Find.find.
+  #
+  #   find '~/Downloads/' do |path|
+  #     # ...
+  #   end
   def find(path, &block)
     Find.find(File.expand_path(path), &block)
   end
 
-  # Made primarily for testability.  Delegates to Kernel.`
+  # Run a shell command.
+  #
+  # Delegates to Kernel.`.  Made primarily for testing other commands.
   def cmd(command)
     %x(#{command})
   end
 
+  # Use Spotlight metadata to determine the site from which a file was downloaded.
+  #
+  #   downloaded_from('foo.zip') # => ['http://www.site.com/foo.zip', 'http://www.site.com/']
   def downloaded_from(path)
     raw = cmd("mdls -raw -name kMDItemWhereFroms #{path.inspect}")
     clean = raw[1, raw.length - 2]
     clean.split(/,\s+/).map { |s| t = s.strip; t[1, t.length - 2] }
   end
 
+  # Use Spotlight metadata to determine audio length.
+  #
+  #   duration_s('foo.mp3') # => 235.705
   def duration_s(path)
     cmd("mdls -raw -name kMDItemDurationSeconds #{path.inspect}").to_f
   end
 
+  # Use Spotlight to locate all files matching the given filename
+  #
+  #   locate('foo.zip') # => ['/a/foo.zip', '/b/foo.zip']
   def locate(name)
     cmd("mdfind -name #{name.inspect}").split("\n")
   end
 
+  # Inspect the contents of a .zip file.
+  #
+  #   zipfile_contents('foo.zip') # => ['foo/foo.exe', 'foo/README.txt']
   def zipfile_contents(path)
     raw = cmd("unzip -Z1 #{path.inspect}")
     raw.split("\n")
   end
 
+  # Calculate disk usage of a given path.
+  #
+  #   disk_usage('foo.zip') # => 136
   def disk_usage(path)
     raw = cmd("du -s #{path.inspect}")
     raw.split(/\s+/).first.to_i
   end
 
+  # Pulls and pushes the given git repository.
+  #
+  #   git_piston('~/code/projectname')
   def git_piston(path)
     full_path = File.expand_path(path)
     stdout = cmd("cd #{full_path.inspect} && git pull && git push 2>&1")
