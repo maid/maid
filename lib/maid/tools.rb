e822cf43b1afe6dfe51aa801818cbc190dcbfcb7
Lloyd Philbrook <stuff@whatafool.com>
2012-09-20 23:57:17 +0800

    updated last_accessed to accessed_at added created_at added modified_at

--- lib/maid/tools.rb
+++ lib/maid/tools.rb
@@ -175,11 +175,26 @@ module Maid::Tools
     end
   end
 
+  # In Unix speak, "ctime".
+  #
+  #   created_at('foo.zip') # => Sat Apr 09 10:50:01 -0400 2011
+  def created_at(path)
+    File.ctime(File.expand_path(path))
+  end
+
   # In Unix speak, "atime".
   #
-  #   last_accessed('foo.zip') # => Sat Apr 09 10:50:01 -0400 2011
-  def last_accessed(path)
+  #   accessed_at('foo.zip') # => Sat Apr 09 10:50:01 -0400 2011
+  def accessed_at(path)
     File.atime(File.expand_path(path))
+  end
+  alias :last_accessed :accessed_at # keep for legacy compatibility.
+
+  # In Unix speak, "mtime".
+  #
+  #   modified_at('foo.zip') # => Sat Apr 09 10:50:01 -0400 2011
+  def modified_at(path)
+    File.mtime(File.expand_path(path))
   end
 
   # Pulls and pushes the given git repository.
