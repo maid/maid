8b08faeec6db3705074c9b082c470494d9e3ea26
Ben Oakes <hello@benjaminoakes.com>
2012-12-21 09:53:36 -0600

    (m) Add wiki link

--- lib/maid/rules.sample.rb
+++ lib/maid/rules.sample.rb
@@ -13,13 +13,14 @@
 #       # ...
 #     end
 # 
-# If you come up with some cool tools of your own, please send me a pull request on GitHub!
+# If you come up with some cool tools of your own, please send me a pull request on GitHub!  Also, please consider sharing your rules with others via [the wiki](https://github.com/benjaminoakes/maid/wiki).
 #
 # For more help on Maid:
 #
 # * Run `maid help`
 # * Read the README, tutorial, and documentation at https://github.com/benjaminoakes/maid#maid
 # * Ask me a question over email (hello@benjaminoakes.com) or Twitter (@benjaminoakes)
+# * Check out how others are using Maid in [the Maid wiki](https://github.com/benjaminoakes/maid/wiki)
 
 Maid.rules do
   # **NOTE:** It's recommended you just use this as a template; if you run these rules on your machine without knowing
