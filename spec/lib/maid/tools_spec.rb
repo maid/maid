d08b80814178122c9d379985d034103356363ef5
Ben Oakes <github@benjaminoakes.com>
2013-12-04 23:01:49 -0600

    (m)

--- spec/lib/maid/tools_spec.rb
+++ spec/lib/maid/tools_spec.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 require 'spec_helper'
 
-# Workaround for Ruby 2.1.0
+# Workaround for Ruby 2.1.0, remove after https://github.com/defunkt/fakefs/pull/209 is released
 if RUBY_VERSION == '2.1.0'
   module FakeFS
     class Dir
