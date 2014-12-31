8def149dbe30cf866c64c9b5ceda81dc0228507e
Benjamin Oakes <hello@benjaminoakes.com>
2014-12-31 12:52:30 -0600

    (m) Move copy next to move, trash, remove, etc

--- lib/maid/tools.rb
+++ lib/maid/tools.rb
@@ -144,6 +144,35 @@ module Maid::Tools
     end
   end
 
+  # Copy from `sources` to `destination`
+  #
+  # The path is not copied if a file already exists at the destination with the same name.  A warning is logged instead.
+  # Note: Similar functionality is provided by the sync tool, but this requires installation of the `rsync` binary
+  # ## Examples
+  #
+  # Single path:
+  #
+  #     copy('~/Downloads/foo.zip', '~/Archive/Software/Mac OS X/')
+  # 
+  # Multiple paths:
+  #
+  #     copy(['~/Downloads/foo.zip', '~/Downloads/bar.zip'], '~/Archive/Software/Mac OS X/')
+  #     copy(dir('~/Downloads/*.zip'), '~/Archive/Software/Mac OS X/')    
+  def copy(sources, destination, options = {})
+    destination = expand(destination)
+
+    expand_all(sources).each do |source|
+        target = File.join(destination, File.basename(source))
+
+      unless File.exist?(target)
+        log("cp #{ source.inspect } #{ destination.inspect }")
+        FileUtils.cp(source, destination )
+      else
+        warn("skipping #{ source.inspect } because #{ target.inspect } already exists")
+      end
+    end
+  end
+
   # Delete the files at the given path recursively.
   #
   # **NOTE**: In most cases, `trash` is a safer choice, since the files will be recoverable by retreiving them from the trash.  Once you delete a file using `remove`, it's gone!  Please use `trash` whenever possible and only use `remove` when necessary.
@@ -655,35 +684,6 @@ module Maid::Tools
   def where_content_type(paths, filter_types)
     filter_types = Array(filter_types)
     Array(paths).select { |p| !(filter_types & content_types(p)).empty? }
-  end
-
-  # Copy from `sources` to `destination`
-  #
-  # The path is not copied if a file already exists at the destination with the same name.  A warning is logged instead.
-  # Note: Similar functionality is provided by the sync tool, but this requires installation of the `rsync` binary
-  # ## Examples
-  #
-  # Single path:
-  #
-  #     copy('~/Downloads/foo.zip', '~/Archive/Software/Mac OS X/')
-  # 
-  # Multiple paths:
-  #
-  #     copy(['~/Downloads/foo.zip', '~/Downloads/bar.zip'], '~/Archive/Software/Mac OS X/')
-  #     copy(dir('~/Downloads/*.zip'), '~/Archive/Software/Mac OS X/')    
-  def copy(sources, destination, options = {})
-    destination = expand(destination)
-
-    expand_all(sources).each do |source|
-        target = File.join(destination, File.basename(source))
-
-      unless File.exist?(target)
-        log("cp #{ source.inspect } #{ destination.inspect }")
-        FileUtils.cp(source, destination )
-      else
-        warn("skipping #{ source.inspect } because #{ target.inspect } already exists")
-      end
-    end
   end
 
   private
