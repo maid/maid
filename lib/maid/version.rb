a7dc5df3de4535d67fa1d3e3febf3f86a7a8b774
Benjamin Oakes <hello@benjaminoakes.com>
2015-07-20 17:45:19 -0500

    Bump version to 0.8.0.alpha.4

--- lib/maid/version.rb
+++ lib/maid/version.rb
@@ -1,4 +1,4 @@
 module Maid
-  VERSION = '0.8.0.alpha.3'
+  VERSION = '0.8.0.alpha.4'
   SUMMARY = 'Be lazy. Let Maid clean up after you, based on rules you define. Think of it as "Hazel for hackers".'
 end
