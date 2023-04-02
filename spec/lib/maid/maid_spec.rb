c1e3c0889898223212b7e31bedac35433b9ba43c
coaxial <c@64b.it>
2023-04-02 10:03:19 +0200

    this flower is scorched

--- spec/lib/maid/maid_spec.rb
+++ spec/lib/maid/maid_spec.rb
@@ -245,12 +245,12 @@ module Maid
       before do
         if Platform.osx?
           FakeFS.deactivate!
-          FileUtils.mkdir_p('zoneinfo/')
-          FileUtils.cp_r('/usr/share/zoneinfo/', 'zoneinfo/')
+          FileUtils.mkdir_p('/tmp/zoneinfo/')
+          FileUtils.cp_r('/usr/share/zoneinfo/', '/tmp/zoneinfo/')
           FakeFS.activate!
-          FakeFS::FileSystem.clone('zoneinfo/')
+          FakeFS::FileSystem.clone('/tmp/zoneinfo/')
           FileUtils.mkdir_p('/usr/share/zoneinfo/')
-          FileUtils.cp_r('zoneinfo/', '/usr/share/zoneinfo/')
+          FileUtils.cp_r('/tmp/zoneinfo/', '/usr/share/zoneinfo/')
         end
         # This is necessary for Rufus to work properly, but since we're using
         # FakeFS, the fake filesystem is missing that file.
