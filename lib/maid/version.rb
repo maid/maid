6415abcbb3a7695747205118a39b59d36c91c413
coaxial <c@64b.it>
2023-05-01 13:33:28 +0200

    Manually bump version

--- lib/maid/version.rb
+++ lib/maid/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Maid
-  VERSION = '0.10.0-alpha.3'
+  VERSION = '0.10.0'
   SUMMARY = 'Be lazy. Let Maid clean up after you, based on rules you define. ' \
             'Think of it as "Hazel for hackers".'
 end
