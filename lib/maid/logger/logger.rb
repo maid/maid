0ba47e5543f612c2a9b328d0518109f4ef02a7f8
coaxial <c@64b.it>
2023-05-05 10:58:16 +0200

    add default progname, log info, remove #device leak

--- lib/maid/logger/logger.rb
+++ lib/maid/logger/logger.rb
@@ -3,22 +3,23 @@ require 'forwardable'
 module Maid
   class Logger
     extend Forwardable
-    attr_reader :device
 
     def_delegators :@logger, :debug, :info, :warn, :error, :fatal, :unknown
 
     def initialize(device:, logger: ::Logger, level: ::Logger::INFO)
-      @device = device
-      create_logfile_dir if device.is_a? String
+      create_logfile_dir(device) if device.is_a? String
+
       @logger = logger.new(device)
+      @logger.progname = 'Maid'
       @logger.level = level
+      @logger.debug(self.class) { "Will log to #{device} with level #{level}" }
     end
 
     private
 
     attr_reader :logger
 
-    def create_logfile_dir
+    def create_logfile_dir(device)
       FileUtils.mkdir_p(File.dirname(device))
     end
   end
