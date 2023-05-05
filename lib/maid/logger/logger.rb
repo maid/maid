a45aef698e296a7dde66dc4e0d63876598e294d7
coaxial <c@64b.it>
2023-05-05 12:16:45 +0200

    add docs

--- lib/maid/logger/logger.rb
+++ lib/maid/logger/logger.rb
@@ -1,11 +1,43 @@
 require 'forwardable'
 
 module Maid
+  # Provides an abstraction over the ::Logger class to streamline logger
+  # instantiation, defaults, and interface.
   class Logger
     extend Forwardable
 
+    # !@method debug
+    #   @see https://docs.ruby-lang.org/en/master/Logger.html
+    # !@method info
+    #   @see https://docs.ruby-lang.org/en/master/Logger.html
+    # !@method warn
+    #   @see https://docs.ruby-lang.org/en/master/Logger.html
+    # !@method error
+    #   @see https://docs.ruby-lang.org/en/master/Logger.html
+    # !@method fatal
+    #   @see https://docs.ruby-lang.org/en/master/Logger.html
+    # !@method unknown
+    #   @see https://docs.ruby-lang.org/en/master/Logger.html
     def_delegators :@logger, :debug, :info, :warn, :error, :fatal, :unknown
 
+    # Creates a new logger
+    # @param device [String, IO] the device to log to
+    # @param logger [::Logger] the logger to use
+    # @param level [Integer] the log level
+    # @return [::Logger] the new logger
+    # @see https://docs.ruby-lang.org/en/master/Logger.html#class-Logger-label-Entries
+    # @example Create a new logger and log an info message
+    #   # with the Maid namespace
+    #   logger = Logger.new(device: '/tmp/my.log')
+    #   logger.info('hello!')
+    #   # or
+    #   logger.info { 'hello!' } # Preferred, see Ruby's Logger docs for details.
+    # @example Log a debug message with the full module's name
+    #   module Maid
+    #     class MyClass
+    #       # ...
+    #       def my_method
+    #   logger.debug(self.class) { 'a message' } # => "Maid::MyClass: a message"
     def initialize(device:, logger: ::Logger, level: ::Logger::INFO)
       create_logfile_dir(device) if device.is_a? String
 
@@ -17,10 +49,12 @@ module Maid
 
     private
 
+    # @return [::Logger] the ::Logger instance
     attr_reader :logger
 
-    def create_logfile_dir(device)
-      FileUtils.mkdir_p(File.dirname(device))
+    # @param filepath [String] full path to the log file
+    def create_logfile_dir(filepath)
+      FileUtils.mkdir_p(File.dirname(filepath))
     end
   end
 end
