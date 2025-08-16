aa5225f6e52d8f3af2ff42dbb91b4c4d83e2af11
coaxial <c@64b.it>
2025-08-16 11:35:55 +0200

    Simplify `prepareCmd`

---
+++ script/update-version.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby                                                  
+version = ARGV[0]
+version_file = 'lib/maid/version.rb'                                 
+
+content = File.read(version_file)                                    
+updated = content.gsub(/VERSION\s*=\s*['"].*?['"]/, "VERSION =       
+'#{version}'")                                                       
+File.write(version_file, updated)                                    
