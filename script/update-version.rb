eccb7b7a7eb2ec7506a3714096b493ce4588a6c6
coaxial <c@64b.it>
2025-08-16 11:48:39 +0200

    Lint file

--- script/update-version.rb
+++ script/update-version.rb
@@ -1,8 +1,8 @@
-#!/usr/bin/env ruby                                                  
+#!/usr/bin/env ruby
 version = ARGV[0]
-version_file = 'lib/maid/version.rb'                                 
+version_file = 'lib/maid/version.rb'
 
-content = File.read(version_file)                                    
-updated = content.gsub(/VERSION\s*=\s*['"].*?['"]/, "VERSION =       
-'#{version}'")                                                       
-File.write(version_file, updated)                                    
+content = File.read(version_file)
+updated = content.gsub(/VERSION\s*=\s*['"].*?['"]/, "VERSION =
+'#{version}'",)
+File.write(version_file, updated)
